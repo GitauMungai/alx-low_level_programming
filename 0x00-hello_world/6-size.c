1bf90689b87aa5cd8b6fd49d3efdf099ae606c1b
GitauMungai <colonelmungai@gmail.com>
2022-09-09 13:19:33 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("%lu\n",sizeof(int));
-	printf("%lu\n",sizeof(char));
-	printf("%lu\n",sizeof(long int));
-	printf("%lu\n",sizeof(long long int));
-	printf("%lu\n",sizeof(float));
-	return(0);
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(long int));
+	printf("%lu\n", sizeof(long long int));
+	printf("%lu\n", sizeof(float));
+	return (0);
 }
