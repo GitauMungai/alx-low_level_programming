046d28240783e3e8c4f12a295e36bdec728a01ca
GitauMungai <colonelmungai@gmail.com>
2022-09-12 14:49:49 -0700

    Reverse order

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+* main - prints numbers of base 10
+* description: prints numbers of base 10 with datatype char
+* Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+	
