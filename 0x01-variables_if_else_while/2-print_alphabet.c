513d3c8022dbf45e4a0bdce444f557b75b777d11
GitauMungai <colonelmungai@gmail.com>
2022-09-11 23:12:59 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+*
+* @my-char: variable
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char my_char = 'a';
+	
+	for (my_char = 'a'; my_char <= 'z'; my_char++)
+	{
+		putchar(my_char);
+		putchar('\n');
+	}
+	return (0);
+}
