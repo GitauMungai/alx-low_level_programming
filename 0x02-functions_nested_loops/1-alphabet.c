cd1b1882cad92eaf76e70e92f93ffca2e33c7bdc
GitauMungai <colonelmungai@gmail.com>
2022-09-14 10:19:59 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints out lowercase alphabet
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	/*calling function _putchar*/
+		_putchar(i);
+	}
+	_putchar('\n');
+	return;
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* main - calls the function print_alphabet
+* Return: 0
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
