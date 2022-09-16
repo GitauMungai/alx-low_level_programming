c896b1017e4ead32611b924ba1576245f43f04e0
GitauMungai <colonelmungai@gmail.com>
2022-09-15 18:01:00 -0700

    24 hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,6 +5,26 @@
 */
 void jack_bauer(void)
 {
-	int i;
+	int hrs = 0;
+	int mins = 0;
+	int mins_rem;
+	int hrs_rem;
 
-	for (i = 
+	while (hrs <= 23)
+	{
+		while (mins <= 59)
+		{
+			hrs_rem = hrs % 10;
+			_putchar(hrs / 10 + '0');
+			_putchar(hrs_rem + '0');
+			_putchar(':');
+			mins_rem = mins % 10;
+			_putchar(mins / 10 + '0');
+			_putchar(mins_rem + '0');
+			mins++;
+			_putchar('\n');
+		}
+		hrs++;
+		mins = 0;
+	}
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* main - calls the jack_bauer
+* Return: 0
+*/
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
