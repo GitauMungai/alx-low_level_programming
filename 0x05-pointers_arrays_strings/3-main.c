4139b844bd6e5f6c0c3356665270f5dc5db620c4
GitauMungai <colonelmungai@gmail.com>
2022-09-19 15:52:45 -0700

    puts

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -4,7 +4,8 @@
 * main - calls the _puts(char *str) function)
 *
 * Return: 0
-*/int main(void)
+*/
+int main(void)
 {
 	char *str;
 
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-* _puts(char *str) - prints a string
+* _puts - prints a string
 *
 * @str: the string to be printed
 */
 void _puts(char *str)
 {
 	puts(str);
-	puts("\n");
 }
