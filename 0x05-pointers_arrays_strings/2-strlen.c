26c0b5e06293b80835504b10520f8b3852fa3366
GitauMungai <colonelmungai@gmail.com>
2022-09-19 15:18:26 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _strlen - prints the length of a string
+*
+* @s: string whose length is being printed
+*
+* Return: int
+*/
+int _strlen(char *s)
+{
+	return strlen(s);
+}
